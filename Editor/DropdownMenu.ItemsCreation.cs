c822e37528c2bb18822e4f11f033baf3310d0073
Lou Garczynski <lgarczyn@student.42.fr>
2025-07-05 20:59:54 +0200

    Update DropdownMenu.ItemsCreation.cs
    Replace the System.Memory Span.Split

--- Editor/DropdownMenu.ItemsCreation.cs
+++ Editor/DropdownMenu.ItemsCreation.cs
@@ -70,4 +70,56 @@
             return parentNode;
         }
     }
+
+    static class SpanExtensions
+    {
+        public static SplitSpanEnumerator Split(this ReadOnlySpan<char> span, char separator)
+        {
+            return new SplitSpanEnumerator(span, separator);
+        }
+
+        public ref struct SplitSpanEnumerator
+        {
+            private ReadOnlySpan<char> _span;
+            private ReadOnlySpan<char> _currentSpan;
+            private readonly char _separator;
+            private int _currentIndex;
+
+            public SplitSpanEnumerator GetEnumerator() => new (_span, _separator);
+
+            public SplitSpanEnumerator(ReadOnlySpan<char> span, char separator)
+            {
+                _span = span;
+                _separator = separator;
+                _currentIndex = 0;
+                _currentSpan = default;
+            }
+
+            public bool MoveNext()
+            {
+                while (_currentIndex < _span.Length && _span[_currentIndex] == _separator)
+                {
+                    _currentIndex++; // Skip leading separators
+                }
+                // If we reached the end of the span, return false
+                if (_currentIndex >= _span.Length)
+                {
+                    return false;
+                }
+                // We known _span[_currentIndex] is not a separator here
+                int nextIndex = _span[_currentIndex..].IndexOf(_separator);
+                // If no separator is found, take the rest of the span
+                if (nextIndex == -1)
+                {
+                    nextIndex = _span.Length - _currentIndex;
+                }
+                // Set the current span to the next segment
+                _currentSpan = _span.Slice(_currentIndex, nextIndex);
+                _currentIndex += nextIndex + 1; // Move past the separator
+                return true;
+            }
+
+            public readonly ReadOnlySpan<char> Current => _currentSpan;
+        }
+    }
 }
